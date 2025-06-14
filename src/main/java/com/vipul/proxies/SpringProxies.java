fc5a998bf91fb85df9c415b392153d61eb750aa5
vipuljainvjj <vipuljain3456@gmail.com>
2025-06-14 22:18:31 +0530

    Refactored SpringProxies

--- src/main/java/com/vipul/proxies/SpringProxies.java
+++ src/main/java/com/vipul/proxies/SpringProxies.java
@@ -10,84 +10,78 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 //@Configuration
 public class SpringProxies {
 
-     static boolean transactional(Object obj) {
-         var hasTransactional = new AtomicBoolean(false);
-         List<Class<?>> allClasses = new ArrayList<>();
-         allClasses.add(obj.getClass());
-         Collections.addAll(allClasses, obj.getClass().getInterfaces());
-//         allClasses.addAll(List.of(obj.getClass().getInterfaces()));
-
-         allClasses.forEach(clzz -> {
-             ReflectionUtils.doWithMethods(clzz, method -> {
-                 if (method.getAnnotation(MyTransactional.class) != null) {
-                     hasTransactional.set(true);
-                 }
-             });
-         });
-         return hasTransactional.get();
-    }
-
-    // callback Interface
-    static class MyTransactionalBeanPostProcessor implements BeanPostProcessor {
-
-        @Override
-        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-            if (bean instanceof CustomerService && transactional(bean)) {
-                ProxyFactory pf = new ProxyFactory();
-                Arrays.stream(bean.getClass().getInterfaces()).map(Class::getName).forEach(System.out::println);
-                pf.setInterfaces(bean.getClass().getInterfaces());
-                pf.setTarget(bean);
-                pf.addAdvice(new MethodInterceptor() {
-
-                    @Override
-                    public Object invoke(MethodInvocation invocation) throws Throwable {
-                        Method method = invocation.getMethod();
-                        Object[] args = invocation.getArguments();
-                        System.out.println("Invocation Handler");
-                        if (method.getAnnotation(MyTransactional.class) != null) {
-                            return method.invoke(bean, args);
-                        }
-                        return null;
-                    }
-                });
-                System.out.println("=====================================================");
-                var proxy = pf.getProxy(bean.getClass().getClassLoader());
-                Arrays.stream(proxy.getClass().getInterfaces()).map(Class::getName).forEach(System.out::println);
-
-                return proxy;
-            }
-            return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
-        }
-    }
-
-    @Bean
-    MyTransactionalBeanPostProcessor transactionalBeanPostProcessor() {
-         return new MyTransactionalBeanPostProcessor();
-    }
-
-    @Bean
-    DefaultCustomerService defaultCustomerService() {
-         return new DefaultCustomerService();
-    }
-
     @Bean
     public ApplicationRunner applicationRunner(CustomerService customerService) {
         return (args) -> {
             customerService.create();
-            customerService.add();
+            customerService.createWithParams("Vipul", "Jain");
         };
+    }
+
+    @Bean
+    DefaultCustomerService defaultCustomerService() {
+        return new DefaultCustomerService();
+    }
+
+    @Bean
+    MyTransactionalBeanPostProcessor transactionalBeanPostProcessor() {
+        return new MyTransactionalBeanPostProcessor();
+    }
+
+    class MyTransactionalBeanPostProcessor implements BeanPostProcessor {
+
+        @Override
+        @NonNull
+        public Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
+            if (bean instanceof CustomerService && isClassTransactional(bean)) {
+                ProxyFactory pf = new ProxyFactory();
+                pf.setInterfaces(bean.getClass().getInterfaces());
+                pf.setTarget(bean);
+                pf.addAdvice(getMethodInterceptor(bean));
+
+                return pf.getProxy(bean.getClass().getClassLoader());
+            }
+            return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+        }
+
+        boolean isClassTransactional(Object obj) {
+            var hasTransactional = new AtomicBoolean(false);
+            List<Class<?>> allClasses = new ArrayList<>();
+            allClasses.add(obj.getClass());
+            Collections.addAll(allClasses, obj.getClass().getInterfaces());
+
+            allClasses.forEach(clzz -> {
+                ReflectionUtils.doWithMethods(clzz, method -> {
+                    if (method.getAnnotation(MyTransactional.class) != null) {
+                        hasTransactional.set(true);
+                    }
+                });
+            });
+            return hasTransactional.get();
+        }
+
+        public MethodInterceptor getMethodInterceptor(Object bean) {
+            return (MethodInvocation invocation) -> {
+                Method method = invocation.getMethod();
+                Object[] args = invocation.getArguments();
+                System.out.println("Invocation Handler is invoked on " + method.getName());
+                if (Objects.nonNull(method.getAnnotation(MyTransactional.class))) {
+                    return method.invoke(bean, args);
+                }
+                return null;
+            };
+        }
     }
 
 }
